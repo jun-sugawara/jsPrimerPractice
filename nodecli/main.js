e56d7c95d5a8a662dceae192459d8d5b6d0bc886
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2023-05-09 08:25:51 +0900

    マークダウンをパースした上でコンソールに表示できるように実装

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,7 +1,26 @@
-// commanderモジュールからprogramオブジェクトをインポートする
 import { program } from "commander";
-// コマンドライン引数をcommanderでパースする
-program.parse(process.argv);
+import * as fs from "node:fs/promises";
+import { marked } from "marked";
 
+program.option("--gfm", "GFMを有効にする");
+program.parse(process.argv);
 const filePath = program.args[0];
-console.log(filePath);
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
+
+// ファイルをUTF-8として非同期で読み込む
+fs.readFile(filePath, { encoding: "utf8" }).then(file => {
+  const html = marked.parse(file, {
+    gfm: cliOptions.gfm,
+  });
+  console.log(html);
+}).catch(err => {
+  console.error(err.message);
+  process.exit(1);
+});
